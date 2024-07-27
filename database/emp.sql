d24521694c1e2533d90b6c9b6ab4a404da5d6443
Akshar Patel <143181114+Aksharpatel06@users.noreply.github.com>
2024-07-27 10:33:03 +0530

    Add files via upload

---
+++ database/emp.sql
@@ -0,0 +1,19 @@
+ -- INSERT INTO employees (name,role,salary,age,phone) VALUES ("Akshar","ceo",100000,19,9727404868);
+-- INSERT INTO employees (name,role,salary,age,phone) VALUES ("Ayush","hr",80000,20,8654312546);
+-- INSERT INTO employees (name,role,salary,age,phone) VALUES ("Meet","manager",75000,20,7986542102);
+-- INSERT INTO employees (name,role,salary,age,phone) VALUES ("Ashupal","emp",80000,18,8541325322);
+-- INSERT INTO employees (name,role,salary,age,phone) VALUES ("Fenish","emp",95000,19,6854138658);
+-- INSERT INTO employees (name,role,salary,age,phone) VALUES ("Darshan","emp",60000,18,8979658888);
+
+-- SELECT * FROM employees;
+-- SELECT name FROM employees;
+-- SELECT salary FROM employees;
+-- SELECT * FROM employees WHERE role = "manager";
+-- SELECT * FROM employees WHERE name LIKE 'A%';
+-- SELECT * FROM employees WHERE (age>18 AND salary>85000);
+
+-- UPDATE employees SET salary = 50000 WHERE id=22;
+-- UPDATE employees SET phone = 6520241545 WHERE id=24;
+
+-- DELETE FROM employees WHERE id=25;
+-- DELETE FROM employees WHERE age>19; 
